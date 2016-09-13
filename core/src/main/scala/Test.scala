b77b7c6e8a7f60934755e75a2806fefce3dcf0cb
yarenty <yarenty@gmail.com>
2016-09-13 16:13:12 +0100

    macros

---
+++ core/src/main/scala/Test.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by yarenty on 13/09/2016.
+  * (C)2015 SkyCorp Ltd.
+  */
+
+@hello
+object Test extends App {
+  println(this.hello)
+}
---
+++ macros/src/main/scala/Macros.scala
@@ -0,0 +1,32 @@
+import scala.reflect.macros.Context
+import scala.language.experimental.macros
+import scala.annotation.StaticAnnotation
+
+/**
+  * Created by yarenty on 13/09/2016.
+  * (C)2015 SkyCorp Ltd.
+  */
+
+
+object helloMacro {
+  def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+    import c.universe._
+    import Flag._
+    val result = {
+      annottees.map(_.tree).toList match {
+        case q"object $name extends ..$parents { ..$body }" :: Nil =>
+          q"""
+            object $name extends ..$parents {
+              def hello: ${typeOf[String]} = "hello"
+              ..$body
+            }
+          """
+      }
+    }
+    c.Expr[Any](result)
+  }
+}
+
+class hello extends StaticAnnotation {
+  def macroTransform(annottees: Any*) = macro helloMacro.impl
+}
